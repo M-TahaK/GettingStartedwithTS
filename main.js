3d531e8750ea83a29e85b86174efcdfcf07963f7
M-TahaK <161716150+M-TahaK@users.noreply.github.com>
2024-03-07 14:22:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var myName;
+myName = "User Died";
+console.log(myName);
+myName = "Taha";
+console.log(myName);
+// Data types in Variables
+var response;
+response = true;
+var student = 'xyz';
---
+++ main.ts
@@ -0,0 +1,10 @@
+let myName
+myName = "User Died"
+console.log(myName);
+myName = "Taha"
+console.log(myName);
+
+// Data types in Variables
+let response: Boolean;
+response = true;
+var student = 'xyz';
